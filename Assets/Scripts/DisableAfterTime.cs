366de055580a46bee5020dae56b920dfde2a4cf0
UtDuong <ut.t.duong@gmail.com>
2017-03-02 22:23:35 -0800

    Feat: Added DisableAfterTime script
    DisableAfterTime script is used to disable a gameObject after a given time.
    -You can use the script by attaching it to any gameObject.
    -Set the LifeInSeconds value in the inspector. This will determine how long the object will stay active.
    -Once the time has elapsed the gameObject will disable itself.

---
+++ Assets/Scripts/DisableAfterTime.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class DisableAfterTime : MonoBehaviour
+{
+    ///<summary> DisableAfterTime will disable the gameObject after _lifeTimer has elapsed.
+    /// Set the "LifeInSeconds" variable in the inspector
+    /// As soon as the gameObject is enabled it will begin 
+    /// to countdown and will disable itself after the time has elapsed.
+    /// </summary>
+
+    //Set in Inspector
+    public float LifeInSeconds;
+
+    //Countdown timer
+    private float _lifeTimer;
+
+    //Property to determine if the time has elapsed
+    private bool TimeElapsed
+    {
+        get { return _lifeTimer <= Time.time; }
+    }
+
+
+    //Initialize timer once the gameObject has been enabled
+    void OnEnable()
+    {
+        _lifeTimer = Time.time + LifeInSeconds;
+    }
+
+    
+    void Update()
+    {
+        //Checks if the time has elapsed
+        if(TimeElapsed)
+            gameObject.SetActive(false); // Disable the gameobject
+    }
+
+}
