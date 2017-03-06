f91f51a687ae28340a7c5f4e18a0c4da82a301de
UtDuong <ut.t.duong@gmail.com>
2017-03-05 22:31:27 -0800

    feat: Add SpawnOnEnable script
    Attached object will spawn select prefab when it is enabled.
    Ex. Used to spawn an explosion when an enemy is spawned.

---
+++ Assets/Scripts/SpawnOnEnable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+///<summary> 
+/// SpawnOnDisable will spawn a gameobject at the same location 
+/// when attached object is enabled.
+/// </summary>
+namespace UtUtils
+{
+    public class SpawnOnEnable : MonoBehaviour
+    {
+
+        public GameObject ObjectToSpawn;
+        private void OnEnable()
+        {
+            if (ObjectToSpawn != null)
+                Instantiate(ObjectToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+}
+
