216fba2a6136cbfa60899cafcdfa11517fc328aa
UtDuong <ut.t.duong@gmail.com>
2017-03-06 23:27:49 -0800

    feat: Add TriggerMessage script
    TriggerMessage allows for the attached object to inform another object when TriggerEnter or TriggerExit
    has been executed. This is useful for door triggers or to wake up sleeping enemies.

---
+++ Assets/Scripts/TriggerMessage.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+///<summary> 
+/// TriggerMessage will send a message to ObjectToInform
+/// Uses OnTriggerEnter and OnTriggerExit to inform object
+/// Set TargetToTriggerWith to the tag you want to trigger with.
+/// </summary>
+
+namespace UtUtils
+{
+    public class TriggerMessage : MonoBehaviour
+    {
+        public GameObject ObjectToInform;
+        public string TriggerEnterMessage;
+        public string TriggerExitMessage;
+        public string TargetToTriggerWith;
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (string.IsNullOrEmpty(TriggerEnterMessage))
+                return;
+            if (other.CompareTag(TargetToTriggerWith))
+                ObjectToInform.SendMessage(TriggerEnterMessage, SendMessageOptions.DontRequireReceiver);
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (string.IsNullOrEmpty(TriggerExitMessage))
+                return;
+            if (other.CompareTag(TargetToTriggerWith))
+                ObjectToInform.SendMessage(TriggerExitMessage, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+}
