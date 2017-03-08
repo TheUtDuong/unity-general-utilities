1536a2bd4db5bed5e76b88bbed3ae9a43a3cbaed
UtDuong <ut.t.duong@gmail.com>
2017-03-07 23:06:03 -0800

    feat: Add DestroyAfterTime script
    Similar to the DisableAfterTime script but instead it would destroy the attached gameObject after the time elapsed.
    Useful if you do not intend to reuse the object later in the scene.

---
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+///<summary> DestroyAfterTime will destroy the gameObject after _lifeTimer has elapsed.
+/// Set the "LifeInSeconds" variable in the inspector
+/// As soon as the gameObject is enabled it will begin 
+/// to countdown and will destroy itself after the time has elapsed.
+/// </summary>
+
+public class DestroyAfterTime : MonoBehaviour {
+
+    //Set in Inspector
+    public float LifeInSeconds;
+
+    //Countdown timer
+    private float _lifeTimer;
+
+    //Property to determine if the time has elapsed
+    private bool TimeElapsed
+    {
+        get { return _lifeTimer <= Time.time; }
+    }
+
+
+    //Initialize timer once the gameObject has been enabled
+    void OnEnable()
+    {
+        _lifeTimer = Time.time + LifeInSeconds;
+    }
+
+
+    void Update()
+    {
+        //Checks if the time has elapsed
+        if (TimeElapsed)
+            Destroy(gameObject); // Destroy the gameobject
+    }
+}
